b0bbe91cb5d2efbcc67ced35539366f29f6a9fe3
PelindungGiawa <61076880+PelindungGiawa@users.noreply.github.com>
2021-12-19 11:43:27 +0700

    Initial Commit

---
+++ SwiftUI_UserLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_UserLocation
+//
+//  Created by Pelindung Giawa on 19/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_UserLocation/SwiftUI_UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserLocationApp.swift
+//  SwiftUI_UserLocation
+//
+//  Created by Pelindung Giawa on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
